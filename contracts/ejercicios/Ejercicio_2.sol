c98a73439fe2fb8d08c8efdd733ea1acf753423c
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2024-05-10 05:14:25 -0400

    Ejecicio 2 listo para comenzar

--- contracts/ejercicios/Ejercicio_2.sol
+++ contracts/ejercicios/Ejercicio_2.sol
@@ -1,49 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-/**
- * Ejercicio 2
- *
- * Vamos a crear una serie de modifiers que nos ayudarán añadir validaciones
- * y protecciones a nuestros contratos.
- *
- * Escenarios a validar/proteger:
- * 1
- * - acceso: que solo el admin sea quien pueda ejecutar una función
- *   nombre modifer: soloAdmin -> Desplegar mensaje "No eres el admin"
- *   aplicar al método: metodoAccesoProtegido
- *
- * 2
- * - permiso: que personas de una lista puedan llamar a un método
- *   nombre modifer: soloListaBlanca
- *   aplicar al método: metodoPermisoProtegido
- *   Adicional:
- *   - definir un setter para incluir addresses en la lista blanca protegido por soloAdmin
- *   - nombre del método: incluirEnListaBlanca
- *
- * 3
- * - tiempo: que un método sea llamado dentro de un rango de tiempo
- *   nombre modifer: soloEnTiempo
- *   aplicar al método: metodoTiempoProtegido
- *
- * 4
- * - pausa: que un método pueda ser pausado y reanudado
- *   nombre modifer: pausa
- *   aplicar al método: metodoPausaProtegido
- *   Adicional:
- *   - definir un método para cambiar ese booleano que tenga el modifier de soloAdmin
- *   - nombre del metodo: cambiarPausa
- *
- *
- * Notas:
- *  - para el modifier de tiempo, se puede usar block.timestamp
- *  - para el modifier de pausa, se puede usar un booleano
- *  - dejar los cuerpos de todos los métodos en blanco
- *
- * Testing: Ejecutar el siguiente comando:
- * - npx hardhat test test/EjercicioTesting_2.js
- */
-
 contract Ejercicio_2 {
     // 1
     // definir un 'admin'
@@ -51,13 +8,13 @@ contract Ejercicio_2 {
     address public admin = 0x08Fb288FcC281969A0BBE6773857F99360f2Ca06;
 
     modifier soloAdmin() {
-        require(msg.sender == admin, "No eres el admin" );
+        // definir logica
         _;
     }
 
-    function metodoAccesoProtegido() public soloAdmin(){
-
-    }
+    // function metodoAccesoProtegido() {
+    //     // ...logica
+    // }
 
     // 2
     // definir lista blanca con un mapping
